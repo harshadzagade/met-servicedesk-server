575427614e45a3ddfe43cb3c52c0b00e4508f32b
Harshad D. Zagade <harshadz@met.edu>
2023-04-13 16:02:10 +0530

    database 2.0

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('service_desk_demo', 'root', 'root', {
+const sequelize = new Sequelize('service_desk_demo', 'root', '', {
     dialect: 'mysql',
     host: 'localhost',
     define: {
